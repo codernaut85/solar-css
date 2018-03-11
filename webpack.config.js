34e19808dd2a099e552a1d139cb6e5dca61a8471
Grant Sykes <grantrjsykes@googlemail.com>
2018-03-11 16:00:44 +0000

    Build progress

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,36 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
 module.exports = {
 	
 	entry: {
 		app: "./index.js",
-		styles: "./sass/index.js"
+		styles: "./src/sass/index.js"
 	},
 
 	output: {
+		path: path.resolve(__dirname, "dist"),
 		filename: "[name].js"
+	},
+
+	watch: true,
+
+	devServer: {
+		publicPath: "/",
+		contentBase: "./dist",
+		hot: true
 	},
 
 	module: {
 		rules: [
 			{ test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] }
 		]
-	}
+	},
+
+	plugins: [
+		new CopyWebpackPlugin([
+			{ from: "static/index.html" }
+		])
+	]
+
 }
